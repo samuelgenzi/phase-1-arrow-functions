56ec7991aa8a9d26bc471081084d169148469fa1
samuel ghilazgi <samuelhaileabg@gmail.com>
2023-06-14 16:04:29 +1000

    my

---
+++ index.js
@@ -0,0 +1,15 @@
+const divide = function(a, b) {
+    return a / b;
+  };
+
+const square = (x) => {
+    return x * x;
+  };
+
+
+const add = (a, b) => {
+    return a + b;
+};                                                                      
+
+
+
